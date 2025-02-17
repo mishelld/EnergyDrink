ce2fcff7c9c40cb7ee696e632865055f14cac08d
mishell <mishelldubovitski@gmail.com>
2025-02-17 10:25:57 +0200

    add scroll animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,76 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Home.css'; // Import the CSS file
 
 function Home() {
+    const imageRefs = useRef([]); // Ref to track all images
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const homeContainer = document.querySelector('.home-container');
+            const homeBottom = homeContainer.getBoundingClientRect().bottom;
+            const homeTop = homeContainer.getBoundingClientRect().top;
+
+            // Trigger the "jump down" animation when `Home` scrolls out of view (downwards)
+            if (homeBottom < window.innerHeight) {
+                imageRefs.current.forEach((img) => {
+                    if (img) {
+                        img.classList.remove('jump-in'); // Remove "jump-in" class if present
+                        img.classList.add('jump-down'); // Add "jump-down" animation
+                    }
+                });
+            }
+
+            // Trigger the "jump in" animation when `Home` scrolls back into view (upwards)
+            if (homeTop > 0 && homeBottom > window.innerHeight) {
+                imageRefs.current.forEach((img) => {
+                    if (img) {
+                        img.classList.remove('jump-down'); // Remove "jump-down" class if present
+                        img.classList.add('jump-in'); // Add "jump-in" animation
+                    }
+                });
+            }
+        };
+
+        // Add scroll listener
+        window.addEventListener('scroll', handleScroll);
+
+        // Cleanup listener
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <div className="home-container">
-            <div className="home-title">Boost </div> {/* Added Title */}
-            <div className="home-subtitle">Up! </div> {/* Added Title */}
+            <div className="home-title">Boost </div>
+            <div className="home-subtitle">Up! </div>
 
             <div className="home-gif-container">
                 <img src="/sugarf.gif" alt="Animated GIF" className="home-gif" />
                 <img src="/electricity.gif" alt="Animated GIF 2" className="home-gif" />
                 <img src="/organi.gif" alt="Animated GIF 3" className="home-gif" />
-
             </div>
+
             <div className="home-image-container">
-                <img 
-                    src="/cherry.png"  // Left image
+                <img
+                    ref={(el) => (imageRefs.current[0] = el)}
+                    src="/cherry.png"
                     alt="Cherry"
                     className="side-image left-image"
                 />
-                <img 
-                    src="/Blueberry.png"  // Main image
+                <img
+                    ref={(el) => (imageRefs.current[1] = el)}
+                    src="/Blueberry.png"
                     alt="Blueberry"
                     className="home-image"
                 />
-                <img 
-                    src="/orange.png"  // Right image
+                <img
+                    ref={(el) => (imageRefs.current[2] = el)}
+                    src="/orange.png"
                     alt="Orange"
                     className="side-image right-image"
                 />
-            </div>            
+            </div>
         </div>
     );
 }
