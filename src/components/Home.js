01af8584448446d601ab6ec7da6977b5b7a62799
mishell <mishelldubovitski@gmail.com>
2025-02-17 10:49:14 +0200

    update scroll animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,24 +11,26 @@ function Home() {
             const homeTop = homeContainer.getBoundingClientRect().top;
 
             // Trigger the "jump down" animation when `Home` scrolls out of view (downwards)
-            if (homeBottom < window.innerHeight) {
-                imageRefs.current.forEach((img) => {
-                    if (img) {
-                        img.classList.remove('jump-in'); // Remove "jump-in" class if present
-                        img.classList.add('jump-down'); // Add "jump-down" animation
-                    }
-                });
-            }
+            // Trigger the "jump down" animation when `Home` scrolls out of view (downwards)
+if (homeBottom < window.innerHeight) {
+    imageRefs.current.forEach((img) => {
+        if (img) {
+            img.classList.remove('jump-in', 'floatUpDown'); // Remove both jump-in and floating
+            img.classList.add('jump-down'); // Add "jump-down" animation
+        }
+    });
+}
 
-            // Trigger the "jump in" animation when `Home` scrolls back into view (upwards)
-            if (homeTop > 0 && homeBottom > window.innerHeight) {
-                imageRefs.current.forEach((img) => {
-                    if (img) {
-                        img.classList.remove('jump-down'); // Remove "jump-down" class if present
-                        img.classList.add('jump-in'); // Add "jump-in" animation
-                    }
-                });
-            }
+// Trigger the "jump in" animation when `Home` scrolls back into view (upwards)
+if (homeTop > 0 && homeBottom > window.innerHeight) {
+    imageRefs.current.forEach((img) => {
+        if (img) {
+            img.classList.remove('jump-down'); // Remove "jump-down" class if present
+            img.classList.add('jump-in', 'floatUpDown'); // Add "jump-in" and floating animation
+        }
+    });
+}
+
         };
 
         // Add scroll listener
@@ -56,19 +58,19 @@ function Home() {
                     ref={(el) => (imageRefs.current[0] = el)}
                     src="/cherry.png"
                     alt="Cherry"
-                    className="side-image left-image"
+                    className="side-image left-image floatUpDown"
                 />
                 <img
                     ref={(el) => (imageRefs.current[1] = el)}
                     src="/Blueberry.png"
                     alt="Blueberry"
-                    className="home-image"
+                    className="home-image floatUpDown"
                 />
                 <img
                     ref={(el) => (imageRefs.current[2] = el)}
                     src="/orange.png"
                     alt="Orange"
-                    className="side-image right-image"
+                    className="side-image right-image floatUpDown"
                 />
             </div>
         </div>
